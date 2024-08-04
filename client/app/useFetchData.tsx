141fee779bcae0011b3c765402c10a5ddea444d1
James Diffey <diffeyj@outlook.com>
2024-08-04 09:29:46 +0100

    fetch working from front end to backend

---
+++ client/app/useFetchData.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const useFetchData = (url) => {
+    const [data, setData] = useState("loading");
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                setData(data.message);
+            })
+            .catch(err => {
+                setData(err.message);
+            });
+    }, [url]);
+
+    return data;
+};
+
+export default useFetchData;
