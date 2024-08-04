81e32ce2ddac6bfa62cd599192beeb170c90b473
James Diffey <diffeyj@outlook.com>
2024-08-04 16:14:00 +0100

    try import of jquery in useEffect

--- client/app/components/LilypadAndWater.tsx
+++ client/app/components/LilypadAndWater.tsx
@@ -1,32 +1,38 @@
 "use client"
 import $ from 'jquery';
-import 'jquery.ripples';
 import { useEffect } from "react";
 import useFetchData from "../useFetchData"; 
 import Overlay from './Overlay';
 
 export default function LilypadAndWater() {
-    // useEffect(() => {
-    //     $('.custom-ripple-effect').ripples({
-    //       resolution: 600,
-    //       perturbance: 0.001,
-    //       dropRadius: 50,
-    //     });
-    //     // Cleanup to destroy the ripple effect on unmount
-    // return () => {
-    //   $('.custom-ripple-effect').ripples('destroy');
-    // };
-  // }, []);
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            // Dynamically import jquery.ripples only in the browser
+            import('jquery.ripples').then(() => {
+                $('.custom-ripple-effect').ripples({
+                    resolution: 600,
+                    perturbance: 0.001,
+                    dropRadius: 50,
+                });
+
+                // Cleanup to destroy the ripple effect on unmount
+                return () => {
+                    $('.custom-ripple-effect').ripples('destroy');
+                };
+            }).catch(err => console.error('Failed to load jquery.ripples:', err));
+        }
+    }, []);
+
 
     return ( // LILYPAD AND WATER BACKGROUDN
         <section className="w-full h-full bg-cover" style={{ backgroundImage: "url('../lilypad.png')" }}>
-            {/* <div className="custom-ripple-effect w-full h-full"> */}
+            <div className="custom-ripple-effect w-full h-full">
 
             {/* LILYPAD OVERLAY ---- and ----- BLACK MENU PLACEMENT STYLES */}
-            {/* <div className="w-full h-full bg-cover z-10" style={{ backgroundImage: "url('../lilypadoverlay.png')" }}>
+            <div className="w-full h-full bg-cover z-10" style={{ backgroundImage: "url('../lilypadoverlay.png')" }}>
                 <Overlay></Overlay>
-            </div> */}
-            {/* </div> */}
+            </div>
+            </div>
         </section>
     )
 }
