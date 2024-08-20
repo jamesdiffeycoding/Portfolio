7d63feca948ba55f349958227c20814c4127c6c3
James Diffey <diffeyj@outlook.com>
2024-08-20 17:54:30 +0100

    test

--- client/app/components/Overlay.tsx
+++ client/app/components/Overlay.tsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 import Links from "./Links";
 export default function Overlay() {
     const awesun = useFetchData("http://127.0.0.1:8080/api/awesun");
-    const shelter = useFetchData("http://localhost:/5000/api/shelter");
+    const shelter = useFetchData("http://localhost:8080/api/shelter");
     const mathemagical = useFetchData("http://127.0.0.1:8080/api/mathemagical");
     const quizzer = useFetchData("http://127.0.0.1:5000/api/quizzer");
   
